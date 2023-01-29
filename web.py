300c5612e1c0e855474f626113382f593ac17284
Polina Staroverova <starowerowa-poly@yandex.ru>
2023-01-29 20:10:33 +0300

    edited web

--- web.py
+++ web.py
@@ -10,6 +10,10 @@ def main_page():
 def about_model():
     return render_template("about_model.html.j2")
 
+@app.route("/testviewer")
+def test_viewer():
+    return render_template("test_viewer.html.j2")
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
