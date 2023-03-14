3c648a1c2d308b4e0d767314d564dc6c3565012f
Polina Staroverova <starowerowa-poly@yandex.ru>
2023-03-14 23:51:31 +0300

    modified

--- web.py
+++ web.py
@@ -10,9 +10,14 @@ def main_page():
 def about_model():
     return render_template("about_model.html.j2")
 
-@app.route("/testviewer")
-def test_viewer():
-    return render_template("test_viewer.html.j2")
+@app.route("/opisanie_lagerya_sobrannogo")
+def opisanie_lagerya_sobrannogo():
+    return render_template("opisanie_lagerya_sobrannogo.html.j2")
+
+@app.route("/kto_mojhet_byt_dobrym_grazhdaninom")
+def kto_mojhet_byt_dobrym_grazhdaninom():
+    return render_template("kto_mojhet_byt_dobrym_grazhdaninom.html.j2")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
