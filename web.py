2f004a72d84b3c3948faff3fce858b613d374ef3
Polina Staroverova <starowerowa-poly@yandex.ru>
2023-03-15 00:04:43 +0300

    modified

--- web.py
+++ web.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return render_template("index.html.j2")
+    return render_template("index.html")
 
 @app.route("/aboutmodel")
 def about_model():
-    return render_template("about_model.html.j2")
+    return render_template("about_model.html")
 
 @app.route("/opisanie_lagerya_sobrannogo")
 def opisanie_lagerya_sobrannogo():
-    return render_template("opisanie_lagerya_sobrannogo.html.j2")
+    return render_template("opisanie_lagerya_sobrannogo.html")
 
 @app.route("/kto_mojhet_byt_dobrym_grazhdaninom")
 def kto_mojhet_byt_dobrym_grazhdaninom():
-    return render_template("kto_mojhet_byt_dobrym_grazhdaninom.html.j2")
+    return render_template("kto_mojhet_byt_dobrym_grazhdaninom.html")
 
 
 if __name__ == '__main__':
